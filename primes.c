ec7eff5cbb4e35dfdc484f911f211f7c0dc7f853
AHerran111 <luis.herran@iteso.mx>
2025-09-09 07:22:51 -0600

    act6

---
+++ primes.c
@@ -0,0 +1,117 @@
+/*
+ * Para compilar hay que agregar la librería matemática
+ *  	gcc -o matprimos matprimos.c -lm
+ *		gcc -o matprimos matprimos.c -lm -lpthread
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+
+#define SIZE 4000
+
+#define INICIAL 900000000
+#define FINAL 1000000000
+#define N_THREADS 4
+
+int mat[SIZE][SIZE];
+
+void initmat(int mat[][SIZE]);
+void printnonzeroes(int mat[SIZE][SIZE]);
+int isprime(int n);
+
+
+
+void * t_func (void *args) {
+
+    int num = *((int*)args);
+    int i,j;
+
+    int start = num * (SIZE/N_THREADS);
+    int end = (num+1) * (SIZE/N_THREADS);
+
+    for(i=start;i<end;i++)
+            for(j=0;j<SIZE;j++)
+                if(!isprime(mat[i][j]))
+                    mat[i][j]=0;
+}
+
+int main()
+{
+	long long start_ts;
+	long long stop_ts;
+	long long elapsed_time;
+	long lElapsedTime;
+	struct timeval ts;
+	int i,j;
+    int num[N_THREADS];
+    pthread_t tid[N_THREADS];
+
+	// Inicializa la matriz con números al azar
+	initmat(mat);
+	
+	gettimeofday(&ts, NULL);
+	start_ts = ts.tv_sec; // Tiempo inicial
+
+	// Eliminar de la matriz todos los números que no son primos
+	// Esta es la parte que hay que paralelizar
+	
+	 for(int i=0; i<N_THREADS; i++){
+        num[i] = i;
+        pthread_create(&tid[i],NULL,t_func,&num[i]);
+    }
+
+    for(int i=0; i<N_THREADS; i++){
+        pthread_join(tid[i],NULL);
+    }
+	
+	// Hasta aquí termina lo que se tiene que hacer en paralelo
+	gettimeofday(&ts, NULL);
+	stop_ts = ts.tv_sec; // Tiempo final
+	elapsed_time = stop_ts - start_ts;
+
+
+	printnonzeroes(mat);
+	printf("------------------------------\n");
+	printf("TIEMPO TOTAL, %lld segundos\n",elapsed_time);
+}
+
+void initmat(int mat[][SIZE])
+{
+	int i,j;
+	
+	srand(getpid());
+	
+	for(i=0;i<SIZE;i++)
+		for(j=0;j<SIZE;j++)
+			mat[i][j]=INICIAL+rand()%(FINAL-INICIAL);
+}
+
+void printnonzeroes(int mat[SIZE][SIZE])
+{
+	int i,j;
+	
+	for(i=0;i<SIZE;i++)
+		for(j=0;j<SIZE;j++)
+			if(mat[i][j]!=0)
+				printf("%d\n",mat[i][j]);
+}
+
+			   
+int isprime(int n)
+{
+	int d=3;
+	int prime=n==2;
+	int limit=sqrt(n);
+	
+	if(n>2 && n%2!=0)
+	{
+		while(d<=limit && n%d)
+			d+=2;
+		prime=d>limit;
+	}
+	return(prime);
+}
