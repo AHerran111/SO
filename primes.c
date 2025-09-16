5e1b48eaab75f6df94e56fdcdd851a6b87deea90
AHerran111 <luis.herran@iteso.mx>
2025-09-16 12:45:24 -0600

    PRACTICA 2 --FALTA TERMINAR TESTING

--- primes.c
+++ primes.c
@@ -6,73 +6,132 @@
 #include <pthread.h>
 #include <time.h>
 
-#define THREADS 12
+//FLAGS AND GLOBAL VALUES
+int THREADS;
+int TEST_FLAG = 0;
+int PAR_FLAG = 1;
+int MIN_THREAD = 0;
+int MAX_THREAD = 1;
+
+//RANGE OF NUMS
 int start;
 int end;
-int count[THREADS] = {0};
 
-int isprime(int n);
+//POINTER TO ARRAY OF PRIME SUMS
+int *count;
+
+//PARSES ARGS WITH OPTARG()
+void parse_args(int argc, char *argv[]);
+//FUNC TO USE WITH pthread()
 void * t_func (void *args);
+//PRIME FUNCTION
+int isprime(int n);
+//MULTITHREAD
+int par_primes();
+//SEQUENTIAL
+int seq_primes();
 
 int main(int argc, char * argv[]) {
-   
-    pthread_t tid[THREADS];
-    int nthread[THREADS];
-    int sum = 0;
-    int i;
+    
+    //STRUCT NEEDED TO COUNT PROCCESING TIME
     struct timespec begin, finish;
-    
+    //HOLDS TOTAL OF PRIMES
+    int sum = 0;
 
+    parse_args(argc,argv);
+    printf("start %d, end %d\n",start,end);
 
-    // printf("La cantidad de args:%d\n", argc);
-    
-    // for(int i=0; i<argc; i++) {
-    //     printf("arg %d:%s\n", i, argv[i]);
-    // }
+    //WHEN TESTING FLAG IS ON MIN/MAX THREAD VALUES ARE ENTERED,
+    //ELSE MIN=0 MAX=1
+    for(int i = MIN_THREAD; i<MAX_THREAD; i++){
 
-    if (argc == 3) {
-        start = atoi(argv[1]);
-        end = atoi(argv[2]);
+        THREADS = i;
+        clock_gettime(CLOCK_MONOTONIC, &begin);//CLOCK BEGINS
 
-        printf("start %d, end %d\n",start,end);
+        if (PAR_FLAG) sum = par_primes();
+        else sum = seq_primes();
 
-        if (end < start) {
-            int temp = start;
-            start = end; end = temp;
-
-        }
-        
-        clock_gettime(CLOCK_MONOTONIC, &begin);
-
-        for(i=0; i<THREADS; i++){
-            nthread[i] = i;
-            pthread_create(&tid[i],NULL,t_func,&nthread[i]);
-        }
-
-        for(i=0; i<THREADS; i++){
-            pthread_join(tid[i],NULL);
-        }
-
-        for(i=0; i<THREADS; i++){
-            sum += count[i];
-        }
-
-        clock_gettime(CLOCK_MONOTONIC, &finish);
-
+        clock_gettime(CLOCK_MONOTONIC, &finish);//CLOCK ENDS
         double elapsed = (finish.tv_sec - begin.tv_sec)
                     + (finish.tv_nsec - begin.tv_nsec) / 1e9;
 
+
+        printf("\nTHREADS USADAS:%d\n",THREADS);
         printf("Cuenta de primes entre %d y %d: %d\n",start,end,sum);
         printf("------------------------------\n");
-	    printf("Elapsed time: %.6f seconds\n", elapsed);
+        printf("Elapsed time: %.6f seconds\n\n", elapsed);
+
+        //count is filled with calloc(), free() needed
+        free(count);
+        count = NULL;
     }
-
-    else
-        printf("help");
-
-
+    
+    
 }
 
+
+void parse_args(int argc, char *argv[]){
+    
+    int opt;
+    int i = 0;
+    
+    //FUNC PARSES ARGS FROM argv[] ON A WHILE LOOP
+    //WITH PREDEFINED OPTIONS "N:hst:"
+    //UNTIL THERE IS NONE, RETURNS -1
+    while((opt = getopt(argc, argv, "N:hst:")) != -1) 
+    { 
+        i += 1;
+        switch(opt) 
+        { 
+            case 'N':
+                printf("N arg detected with value %s\n",optarg);
+                THREADS = atoi(optarg);
+                break;
+
+            case 'h':
+                printf("help usage:banderas primero, nums al final\n");
+                exit(0);
+
+            case 's':
+                printf("Sequencial mode on,multithreads disabled\n");
+                PAR_FLAG = 0;
+                break;
+
+            case 't':
+                printf("TESTING ENABLED\n");
+                sscanf(optarg, "%d..%d", &MIN_THREAD, &MAX_THREAD);
+                TEST_FLAG = 1;
+                break;
+
+            case '?':
+                printf("help usage:banderas primero, nums al final\n");
+                exit(0);
+        } 
+    } 
+
+    //EDGE CASES/ARG ORDER DIFFERENT AS EXPECTED
+    if(!i && argc > 3) {
+        printf("unknown input entered\n");
+        printf("help usage:banderas primero, despues nums\n");
+        exit(0);
+    }
+
+    start = atoi(argv[argc-2]);
+    end = atoi(argv[argc-1]);
+
+    if(start > 2147483647 || end > 2147483647){
+        printf("Start or end value out of bounds\n");
+        exit(0);
+    }
+
+    if (end < start) {
+        int temp = start;
+        start = end; end = temp;
+    }
+}
+
+//CALLS isprime() ON DETERMINED RANGE SET BY
+//thread_num AND THREADS DEFINED
 void * t_func (void *args) {
     int nthread = *(int*)args;
     int range = (end - start);
@@ -80,13 +139,16 @@ void * t_func (void *args) {
     int first = start + (nthread * range/THREADS); 
     int last = start + ((nthread + 1) * range/THREADS);
 
-    if(nthread ==THREADS - 1) last += 1;
+    //NEEDED SO LAST THREAD RUN DOESNT SKIP LAST NUM
+    if(nthread == THREADS - 1) last += 1;
 
     for(int i = first; i<last; i++) {
         if(isprime(i)) count[nthread] += 1;
     }
     
+    return NULL;
 }
+
 
 
 int isprime(int n)
@@ -102,4 +164,48 @@ int isprime(int n)
 		prime=d>limit;
 	}
 	return(prime);
+}
+
+
+int seq_primes(){
+    int sum = 0;
+
+    for(int i = start; i<end; i++) {
+        if(isprime(i)) sum += 1;
+    }
+
+    return sum;
+}
+
+
+int par_primes(){
+    
+    //CHECKS AMMOUNT OF THREADS POSSIBLE IN SYS IF NOT PREV DEFINED
+    if (!THREADS && !TEST_FLAG) THREADS = sysconf(_SC_NPROCESSORS_ONLN);
+
+    count = calloc(THREADS, sizeof(int));
+    pthread_t tid[THREADS];//HOLDS threadId
+    //HOLDS thread_num SO EACH t_func KNOWS WHO IS WHO
+    int nthread[THREADS];
+    int sum = 0;
+    int i = 0;
+
+    //OPENS THREADS
+    for(i=0; i<THREADS; i++){
+        nthread[i] = i;
+        pthread_create(&tid[i],NULL,t_func,&nthread[i]);
+    }
+
+    //CLOSES THREADS
+    for(i=0; i<THREADS; i++){
+        pthread_join(tid[i],NULL);
+    }
+
+    //SUMS EACH TREAD PRIME COUNT
+    for(i=0; i<THREADS; i++){
+        sum += count[i];
+    }
+
+    
+    return sum;
 }
